1a0eda3aedd7aee1e08d28be3cdd1d61ba7a975e
mariusz-modzelewski <m.modzelewski97@gmail.com>
2020-12-21 23:05:54 +0100

    add js for hamburger and finish rwd

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,29 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger--active');
     menu.classList.toggle('menu__items--open');
 });
+
+const hideHamburger = (isDesktop) => {
+    if (isDesktop) {
+        menu.classList.remove('menu__items--open');
+        hamburger.classList.remove('hamburger--active');
+    }
+}
+
+let desktopViewport = window.matchMedia("screen and (min-width: 1024px)");
+
+desktopViewport.addListener(isDesktop => {
+    hideHamburger(isDesktop.matches);
+});
+
+
+var menuFixed = document.querySelector(".menu");
+
+wrap.on("scroll", function (e) {
+
+    if (this.scrollTop > 147) {
+        wrap.addClass("fix-search");
+    } else {
+        wrap.removeClass("fix-search");
+    }
+
+});
