aed3d4da6bac90d8b9bddcbb1c36040e9b67b395
mariusz-modzelewski <m.modzelewski97@gmail.com>
2021-02-21 12:07:14 +0100

    add typing effect and alt for skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,51 @@ window.addEventListener("scroll", () => {
         document.querySelector(".menu__brand").src = "/assets/img/logo.png";
     }
 });
+
+// Typing effect
+const typingBoxes = [document.querySelector(".header__sub-title"), document.querySelector(".header__title")];
+
+const typingText = ["Cześć! Jestem Mariusz", "PrestaShop ^Developer", "Front-end ^Developer"];
+
+let activeDOMElement = typingBoxes[0];
+
+let wordIndex = 0;
+let textIndex = 0;
+let oldTime = 0;
+const speed = 70;
+const stop = 1500;
+
+activeDOMElement.classList.add("cursor");
+const typing = (newTime) => {
+    if (newTime - oldTime > speed) {
+        const letter = typingText[textIndex].substr(wordIndex, 1);
+        if (wordIndex === typingText[textIndex].length) {
+            if (textIndex === typingText.length - 1) {
+                return;
+            }
+            return setTimeout(() => {
+                textIndex++;
+                activeDOMElement.classList.remove("cursor");
+                activeDOMElement = typingBoxes[1];
+                activeDOMElement.classList.add("cursor");
+                if (activeDOMElement === typingBoxes[1]) {
+                    activeDOMElement.textContent = "";
+                }
+                wordIndex = 0;
+                requestAnimationFrame(typing);
+            }, stop)
+        } else if (letter === "^") {
+            const span = document.createElement('span');
+            activeDOMElement.appendChild(span);
+            activeDOMElement = span;
+        }
+
+        if (!(letter === "^")) {
+            activeDOMElement.textContent += letter;
+        }
+        oldTime = newTime;
+        wordIndex++;
+    }
+    requestAnimationFrame(typing);
+}
+typing();
